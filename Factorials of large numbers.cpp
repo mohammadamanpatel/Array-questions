dd3f3d92fee7b0f0a2e544c7408e8b5dc2decbc2
mohammadamanpatel <125564920+mohammadamanpatel@users.noreply.github.com>
2023-02-24 15:58:53 +0530

    Create Factorials of large numbers.cpp

---
+++ Factorials of large numbers.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> factorial(int N){
+        vector<int>ans;
+        ans.push_back(1);
+        int carry = 0;
+        for(int i=2;i<=N;i++)
+        {
+            for(int j=0;j<ans.size();j++)
+            {
+               int x = ans[j]*i+carry;
+                ans[j] = x%10;
+                carry = x/10;
+            }
+       while(carry)
+        {
+            ans.push_back(carry%10);
+            carry/=10;
+        }
+    }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        vector<int> result = ob.factorial(N);
+        for (int i = 0; i < result.size(); ++i){
+            cout<< result[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
